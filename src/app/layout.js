6590f95f0532103f617006d63b12b3f41df378c5
hmweb77 <oussamahamdani1@gmail.com>
2024-06-13 15:41:41 +0100

    add google analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,7 @@ import Script from "next/script";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "wavea",
+  title: "Wavea",
   description: "Riding the next digital waves technology",
 };
 
@@ -13,14 +13,28 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-P07WD7HTWK"></Script>
+      {/* <Script async src="https://www.googletagmanager.com/gtag/js?id=G-P07WD7HTWK"></Script>
 <Script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments)}
   gtag('js', new Date());
 
   gtag('config', 'G-P07WD7HTWK');
+</Script> */}
+
+<Script 
+id="google-tag-manager"
+strategy="afterInteractive"
+dangerouslySetInnerHTML={{
+  __html:`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-W2Q2DJGW');`
+}}>
+
 </Script>
+
       </head>
       <body className={inter.className}>{children}</body>
     </html>
